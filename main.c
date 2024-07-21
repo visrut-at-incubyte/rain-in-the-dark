698ae1768aa16f55a64e2b09bb5b42998add0e61
visrut <visrut@incubyte.co>
2024-07-21 18:37:21 +0530

    circle is moving with radius

--- main.c
+++ main.c
@@ -5,17 +5,45 @@ int main(void)
     int screenWidth = 800;
     int screenHeight = 450;
 
+    Vector2 ballPosition =  { (float)screenWidth/2, (float)screenHeight/2 };
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);
 
+    int radius = 50;
+
     while (!WindowShouldClose())
     {
+
+        if(IsKeyDown(KEY_DOWN)) {
+            ballPosition.y += 5.0f;
+            radius -= 1;
+        }
+        if(IsKeyDown(KEY_UP)) {
+            ballPosition.y -= 5.0f;
+            radius += 1;
+        }
+        if(IsKeyDown(KEY_RIGHT)) {
+            ballPosition.x += 5.0f;
+            radius += 1;
+        }
+        if(IsKeyDown(KEY_LEFT)) {
+            ballPosition.x -= 5.0f;
+            radius -= 1;
+        }
+
+        if(IsKeyDown(KEY_Q)) {
+            break;
+        }
+
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, RAYWHITE);
+        DrawCircleV(ballPosition, radius, MAROON);
+
+        
 
         EndDrawing();
     }
