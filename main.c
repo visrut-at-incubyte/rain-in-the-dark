4bd627ab5eb6c02c181aefa313793f301e927b59
visrut <visrut@incubyte.co>
2024-07-22 09:31:19 +0530

    just black window

--- main.c
+++ main.c
@@ -1,54 +1,24 @@
 #include "raylib.h"
 
-int main(void)
-{
-    int screenWidth = 800;
-    int screenHeight = 450;
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
 
-    Vector2 ballPosition =  { (float)screenWidth/2, (float)screenHeight/2 };
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+int main() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Real Rain");
 
     SetTargetFPS(60);
 
-    int radius = 50;
 
-    while (!WindowShouldClose())
-    {
-
-        if(IsKeyDown(KEY_DOWN)) {
-            ballPosition.y += 5.0f;
-            radius -= 1;
-        }
-        if(IsKeyDown(KEY_UP)) {
-            ballPosition.y -= 5.0f;
-            radius += 1;
-        }
-        if(IsKeyDown(KEY_RIGHT)) {
-            ballPosition.x += 5.0f;
-            radius += 1;
-        }
-        if(IsKeyDown(KEY_LEFT)) {
-            ballPosition.x -= 5.0f;
-            radius -= 1;
-        }
-
+    while(!WindowShouldClose()) {
         if(IsKeyDown(KEY_Q)) {
             break;
         }
 
         BeginDrawing();
-
         ClearBackground(BLACK);
-
-        DrawCircleV(ballPosition, radius, MAROON);
-
-        
-
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-cc main.c -I./raylib-5.0_linux_amd64/include -L./raylib-5.0_linux_amd64/lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+file_name=$1
+
+cc $file_name -I./raylib-5.0_linux_amd64/include -L./raylib-5.0_linux_amd64/lib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -Wall -Werror
+
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+else
+    echo "Compilation failed"
+    exit 1
+fi
+
 LD_LIBRARY_PATH=./raylib-5.0_linux_amd64/lib:$LD_LIBRARY_PATH ./a.out
