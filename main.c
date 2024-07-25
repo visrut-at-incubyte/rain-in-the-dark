ba3a3c93821c17233cba0d3205ecb9ad65a84e57
visrut <visrut@incubyte.co>
2024-07-25 16:01:17 +0530

    show t-shirt

--- main.c
+++ main.c
@@ -5,50 +5,29 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
-#define RAIN_ANGLE 75
-
-float CovertDegreeToRadian(float degree);
-
-typedef struct Particle {
-  Vector2 startPosition;
-  int length;
-} Particle;
-
-Vector2 getEndPosition(Vector2 startPosition, float angle, int length) {
-  Vector2 endPosition;
-  endPosition.x = startPosition.x + length * cos(CovertDegreeToRadian(angle));
-  endPosition.y = startPosition.y + length * sin(CovertDegreeToRadian(angle));
-  return endPosition;
-}
 
 int main() {
-  float speedX = 0;
-  float speedY = 0;
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Real Rain");
 
-  SetTargetFPS(60);
+  Image image = LoadImage("T-Shirt.png");
+  Texture2D texture = LoadTextureFromImage(image);
+  UnloadImage(image);
 
-  Particle particle;
-  particle.startPosition = (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-  particle.length = GetRandomValue(5, 30);
+  SetTargetFPS(30);
 
   while (!WindowShouldClose()) {
     if (IsKeyDown(KEY_Q)) {
       break;
     }
 
+    printf("frame time: %f\n", GetFrameTime());
+
     BeginDrawing();
     ClearBackground(BLACK);
 
-    Vector2 startPosition = particle.startPosition;
-    Vector2 endPosition =
-        getEndPosition(particle.startPosition, RAIN_ANGLE, particle.length);
-
-    DrawLine(startPosition.x + speedX, startPosition.y + speedY,
-             endPosition.x + speedX, endPosition.y + speedY, WHITE);
-
-    speedX += cos(CovertDegreeToRadian(RAIN_ANGLE));
-    speedY += sin(CovertDegreeToRadian(RAIN_ANGLE));
+    DrawTexture(texture, SCREEN_WIDTH / 2 - texture.width / 2,
+                SCREEN_HEIGHT / 2 - texture.height / 2, WHITE);
+    DrawText("this IS a texture loaded from an image!", 300, 370, 10, GRAY);
 
     EndDrawing();
   }
